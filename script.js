45fd2fdb1057add34e4ffe235cb33df787093bcd
Isaac Lins <isaac.lins07@gmail.com>
2024-06-17 09:25:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const background = document.querySelector('.background');
-    const numberOfHearts = 80;
-    const animationDuration = 10; // seconds
+    const numberOfHearts = 100;
+    const animationDuration = 10; 
 
     for (let i = 0; i < numberOfHearts; i++) {
         createHeart();
@@ -14,13 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const randomLeft = Math.random() * 100;
     const randomDelay = Math.random() * animationDuration;
-    const randomTilt = Math.random() * 20 - 10; // Random tilt between -10 and 10 degrees
-    const randomDuration = Math.random() * 5 + 5; // Random duration between 5 and 10 seconds
+    const randomTilt = Math.random() * 20 - 10;
+    const randomDuration = Math.random() * 5 + 5;
 
     heart.style.left = `${randomLeft}vw`;
-    heart.style.bottom = `0`; // Start from the bottom of the screen
+    heart.style.bottom = `0`; 
     heart.style.animationDelay = `${randomDelay}s`;
-    heart.style.animationDuration = `${randomDuration}s`; // Set the animation duration
+    heart.style.animationDuration = `${randomDuration}s`; 
     heart.style.transform = `rotate(${randomTilt}deg)`;
 
     background.appendChild(heart);
